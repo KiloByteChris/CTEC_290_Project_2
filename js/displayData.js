5f3a987fb08803434887fbfc26bfe2e87ac08abe
c.mcguire <cdmcguire000@gmail.com>
2018-02-21 13:02:08 -0800

    display.js changes

--- js/displayData.js
+++ js/displayData.js
@@ -1,13 +1,35 @@
-function displayData(data) {
+function displayData(data, table) {
 	var displayString = "";
 	displayString += "<table>";
-	$.each(data, function(key, value){
-		displayString += "<tr>";
-		//"<td>"++"</td>
-		var lastName = value.last_name;
-		displayString += "<td><a href=\"\">Edit</a></td><td><a href=\"\">Delete</a></td><td>"+value.customer_number+"</td><td>"+value.date1+"</td><td>"+value.first_name+"</td><td>"+value.last_name+"</td><td>"+value.city_name+"</td><td>"+value.state_name+"</td><td>"+value.country_name+"</td>";
-		displayString += "</tr>";
-	});
+	switch(table) {
+		case "table1":
+			displayString += "<tr><th></th><th></th><th>Customer Number</th><th>Date</th><th>First Name</th><th>Last Name</th><th>City</th><th>Region</th><th>country</th></tr>";
+			$.each(data, function(key, value){
+				displayString += "<tr>";
+				//"<td>"++"</td>
+				displayString += "<td><a href=\"\">Edit</a></td><td><a href=\"\">Delete</a></td><td>"+value.customer_number+"</td><td>"+value.date1+"</td><td>"+value.first_name+"</td><td>"+value.last_name+"</td><td>"+value.city_name+"</td><td>"+value.state_name+"</td><td>"+value.country_name+"</td>";
+				displayString += "</tr>";
+			});
+			break
+		case "table2":
+			displayString += "<tr><th></th><th></th><th>Product Number</th><th>Date</th><th>Original Price</th><th>Regular Price</th><th>Sale Price</th><th>On Hand</th><th>Description</th></tr>";
+			$.each(data, function(key, value){
+				displayString += "<tr>";
+				//"<td>"++"</td>
+				displayString += "<td><a href=\"\">Edit</a></td><td><a href=\"\">Delete</a></td><td>"+value.product_number+"</td><td>"+value.date2+"</td><td>"+value.original_price+"</td><td>"+value.regular_price+"</td><td>"+value.sale_price+"</td><td>"+value.on_hand+"</td><td>"+value.description+"</td>";
+				displayString += "</tr>";
+			});
+			break
+		case "table3":
+			displayString += "<tr><th></th><th></th><th>Customer ID</th><th>Credt Card Number</th><th>Address</th><th>Number Ordered</th><th>Reference Number</th><th>Product</th><th>Description</th></tr>";
+			$.each(data, function(key, value){
+				displayString += "<tr>";
+				//"<td>"++"</td>
+				displayString += "<td><a href=\"\">Edit</a></td><td><a href=\"\">Delete</a></td><td>"+value.customer_id+"</td><td>"+value.credit_card_num+"</td><td>"+value.address+"</td><td>"+value.number_ordered+"</td><td>"+value.reference_number+"</td><td>"+value.product_ordered+"</td><td>"+value.product_description+"</td>";
+				displayString += "</tr>";
+			});
+			break
+		}
 	displayString += "</table>";
 	$("#dataDisplayDiv").append(displayString);
 }
